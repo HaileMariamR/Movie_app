0886358dc94e5b825473a93e248b504c7fb314ba
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-22 10:16:16 +0300

    working on search bar

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utilities/constants.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Home",
+          style: KappbarTextStyle,
         ),
+      ),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Expanded(
+              child: TextField(),
+              flex: 1,
+            ),
+          ),
+          Expanded(
+              flex: 4,
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.h_mobiledata),
+                  )
+                ],
+              ))
+        ],
       ),
     );
   }
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kTempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 100.0,
-);
-
-const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 60.0,
-);
-
-const kButtonTextStyle = TextStyle(
+const KappbarTextStyle = TextStyle(
   fontSize: 30.0,
-  fontFamily: 'Spartan MB',
-);
-
-const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
 );
