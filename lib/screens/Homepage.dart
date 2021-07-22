fb9ae867427a10a8c1fbb036c8a96ef5ec0a38d1
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-22 09:45:23 +0300

    setup project architecture

---
+++ lib/screens/Homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+      ),
+    );
+  }
+}
